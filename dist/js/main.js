562b7c1f0ef5fd77735c2e1d3f42b121004ef24a
unknown <mhassan4392@gmail.com>
2019-03-09 00:04:26 +0500

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,23 @@
+let navbarBtn = document.querySelector('.navbar-btn');
+let sideNavbar = document.querySelector('.side-navbar');
+let pageContainer = document.querySelector('.page-container');
+let mainNavbar = document.querySelector('#top-navbar');
+//let header = document.querySelector('#main-header');
+
+navbarBtn.addEventListener('click', function(){
+    sideNavbar.classList.toggle('d-block');
+    pageContainer.classList.toggle('change-page-width');
+    //header.classList.toggle('change-page-width');
+    navbarBtn.classList.toggle('navbar-btn-active');
+});
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      mainNavbar.classList.add('scroll-navbar');
+    } else {
+        mainNavbar.classList.remove('scroll-navbar');
+    }
+  }
