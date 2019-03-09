e92428002d38006003e5dc89246500b2a626ce57
unknown <mhassan4392@gmail.com>
2019-03-09 16:43:52 +0500

    Updates

--- js/main.js
+++ js/main.js
@@ -3,15 +3,35 @@ let sideNavbar = document.querySelector('.side-navbar');
 let pageContainer = document.querySelector('.page-container');
 let mainNavbar = document.querySelector('#top-navbar');
 let banner = document.querySelector('#homeTopBanner');
+let dropdownLi = document.querySelectorAll('.dropdown-li');
+let dropdownList = document.querySelectorAll('.dropdown-list');
 //let header = document.querySelector('#main-header');
-console.log(banner);
+// console.log(banner);
 
 navbarBtn.addEventListener('click', function(){
     sideNavbar.classList.toggle('d-block');
-    pageContainer.classList.toggle('change-page-width');
-    banner.classList.toggle('change-page-width');
+    //pageContainer.classList.toggle('change-page-width');
+    //banner.classList.toggle('change-page-width');
     //header.classList.toggle('change-page-width');
     navbarBtn.classList.toggle('navbar-btn-active');
+
+    // banner.forEach(function(el){
+    //   el.classList.toggle('change-page-width');
+    // })
+});
+
+dropdownLi.forEach(function(el){
+  el.addEventListener('click', function(e){
+    if(e.target.classList.contains('dropdown-link')){
+      e.target.nextSibling.nextSibling.classList.toggle('d-block');
+      
+    }
+     
+    if(e.target.classList.contains('dropdown-li')){
+      e.target.children[1].classList.toggle('d-block');
+    }
+    
+  });
 });
 
 
